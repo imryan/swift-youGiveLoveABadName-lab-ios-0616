a372a4789ccb28e3e0c1b5695a2c19e08b0fe2ce
imryan <notryancohen@gmail.com>
2016-07-15 11:01:02 -0400

    done

--- YouGiveLoveABadName/YouGiveLoveABadName/ViewController.swift
+++ YouGiveLoveABadName/YouGiveLoveABadName/ViewController.swift
@@ -10,14 +10,88 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var button: UIButton!
+    
+    @IBOutlet weak var topHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var bottomHeightConstraint: NSLayoutConstraint!
+    
+    var initialConstant: CGFloat = 0
+    
+    var stretched: Bool {
+        return (topHeightConstraint.constant > 470)
     }
     
     @IBAction func expandButtonTapped(sender: AnyObject) {
-        
+        if (!stretched) {
+            expand()
+        } else {
+            shrink()
+        }
     }
-
+    
+    func expand() {
+        UIView.animateKeyframesWithDuration(0.4, delay: 0.0, options: .CalculationModePaced, animations: {
+            self.view.layoutIfNeeded()
+            
+            UIView.addKeyframeWithRelativeStartTime(0.0, relativeDuration: 1.0, animations: {
+                self.topHeightConstraint.constant -= 50
+                self.view.layoutIfNeeded()
+            })
+            
+            UIView.addKeyframeWithRelativeStartTime(0.0, relativeDuration: 0.2, animations: {
+                self.topHeightConstraint.constant = (self.view.frame.size.height + 100)
+                self.view.layoutIfNeeded()
+            })
+            
+            UIView.addKeyframeWithRelativeStartTime(0.0, relativeDuration: 0.5, animations: {
+                self.topHeightConstraint.constant -= 100
+                self.view.layoutIfNeeded()
+            })
+            
+            }, completion: { finished in
+                if (finished) {
+                    //
+                }
+        })
+    }
+    
+    func shrink() {
+        UIView.animateKeyframesWithDuration(0.4, delay: 0.0, options: .CalculationModePaced, animations: {
+            self.view.layoutIfNeeded()
+            
+            UIView.addKeyframeWithRelativeStartTime(0.0, relativeDuration: 1.0, animations: {
+                self.topHeightConstraint.constant += 100
+                self.view.layoutIfNeeded()
+            })
+            
+            UIView.addKeyframeWithRelativeStartTime(0.0, relativeDuration: 0.2, animations: {
+                self.topHeightConstraint.constant = self.initialConstant
+                self.view.layoutIfNeeded()
+            })
+            
+            UIView.addKeyframeWithRelativeStartTime(0.0, relativeDuration: 0.5, animations: {
+                self.topHeightConstraint.constant -= 50
+                self.view.layoutIfNeeded()
+            })
+            
+            UIView.addKeyframeWithRelativeStartTime(0.0, relativeDuration: 0.5, animations: {
+                self.topHeightConstraint.constant = self.initialConstant
+                self.view.layoutIfNeeded()
+            })
+            
+            }, completion: { finished in
+                if (finished) {
+                    //
+                }
+        })
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.bringSubviewToFront(button)
+        
+        initialConstant = topHeightConstraint.constant
+    }
 }
 
